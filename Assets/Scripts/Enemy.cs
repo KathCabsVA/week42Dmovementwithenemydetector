2be69525cf2907f4208e1ca1f6f9025deeca6047
CIIT601PC01\Student <it@ciit.edu.ph>
2024-05-20 10:13:10 +0800

    Updated Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -20,6 +21,17 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //when we reach target
+        if(Vector3.Distance(transform.position, target) < 0.5f) 
+        {
+
+            //at target, pick up new spot to go to
+            Quaternion rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f,360.0f)); //random movement in 360
+            target = lastSeen + (rotation * transform.up * 5.0f);
+        }
+        else
+        {
+            Vector3 dir = target + transform.position;
+        }
     }
 }
